87e3268cc74635c9b7eff7b6a798a32e52d1f2c0
patharanordev <patharanor.dev@gmail.com>
2016-04-18 18:16:20 +0700

    Update ema.js

--- ema.js
+++ ema.js
@@ -1,50 +1,4 @@
-
-var exponentialMovingAVG = function(dataArray, focusPriceIndex, timePeriods, resultArr){
-	var result = -1;
-
-	try {
-		// Smoothing factor
-		var alpha = parseFloat(2.0 / (1 + timePeriods));
-
-		if(focusPriceIndex < dataArray.length){
-			if(focusPriceIndex > timePeriods-1){
-				if(focusPriceIndex-1 > -1){
-					var prevEMA = 0;
-					prevEMA = this.exponentialMovingAVG(dataArray, (focusPriceIndex-1), timePeriods, resultArr);
-
-					if(prevEMA > -1){
-						result = (dataArray[focusPriceIndex] * alpha) + (prevEMA * (1 - alpha));
-
-						if(resultArr instanceof Array)
-							resultArr.push(result);
-
-						focusPriceIndex -= 1;
-					} else {
-						result = -1;
-					}
-				}
-			} else if(focusPriceIndex == timePeriods - 1){
-				result = this.simpleMovingAVG(dataArray, timePeriods);
-
-				if(resultArr instanceof Array) {
-					for(var i=0;i<focusPriceIndex;i++){
-						resultArr.push(0);
-					}
-
-					resultArr.push(result);
-				}
-			} 
-		}
-	} catch(err) {
-		result = -1;
-		console.log("EMA Error : " + err);
-	}
-
-	return result;
-};
-
-
-var exponentialMovingAVGWithObject = function(dataObjArray, focusPriceIndex, timePeriods){
+var exponentialMovingAVG= function(dataObjArray, focusPriceIndex, timePeriods){
 	var result = false;
 
 	try {
@@ -55,25 +9,21 @@ var exponentialMovingAVGWithObject = function(dataObjArray, focusPriceIndex, tim
 			if(focusPriceIndex > timePeriods-1){
 				if(focusPriceIndex-1 > -1){
 
-					result = this.exponentialMovingAVG(dataObjArray, (focusPriceIndex-1), timePeriods);
+					exponentialMovingAVG(dataObjArray, (focusPriceIndex-1), timePeriods);
 					//console.log('prevEMA#' + (focusPriceIndex-1) + ' : ' + dataObjArray[focusPriceIndex-1].ema);
 
-					if(result!=false){
-						dataObjArray[focusPriceIndex].ema = (dataObjArray[focusPriceIndex].data * alpha) + (dataObjArray[focusPriceIndex-1].ema * (1 - alpha));
-						//console.log('EMA ' + dataObjArray[focusPriceIndex].date + ' : ' + dataObjArray[focusPriceIndex].ema);
-						focusPriceIndex -= 1;
-
-						result = dataObjArray[focusPriceIndex].ema;
-					} else {
-						result = false;
-					}
+					dataObjArray[focusPriceIndex].ema = (dataObjArray[focusPriceIndex].data * alpha) + (dataObjArray[focusPriceIndex-1].ema * (1 - alpha));
+					//console.log('EMA ' + dataObjArray[focusPriceIndex].date + ' : ' + dataObjArray[focusPriceIndex].ema);
+					focusPriceIndex -= 1;
 				}
 			} else if(focusPriceIndex == timePeriods - 1){
-				var sma = this.simpleMovingAVGWithObject(dataObjArray, timePeriods);
+				var sma = simpleMovingAVG(dataObjArray, timePeriods);
 
 				if(sma!=false) {
 					dataObjArray[focusPriceIndex].ema = sma;
 					result = sma;
+				} else {
+					dataObjArray[focusPriceIndex].ema = 0;
 				}
 			} 
 		}
@@ -83,4 +33,4 @@ var exponentialMovingAVGWithObject = function(dataObjArray, focusPriceIndex, tim
 	}
 
 	return result;
-}
+};
